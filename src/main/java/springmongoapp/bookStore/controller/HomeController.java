2ebde39b11d6a4c5b037ff62fe6ab94000abe9d3
steyer91 <30693580+steyer91@users.noreply.github.com>
2023-01-29 00:09:59 +0100

    Update HomeController.java
    Refactoring

--- src/main/java/springmongoapp/bookStore/controller/HomeController.java
+++ src/main/java/springmongoapp/bookStore/controller/HomeController.java
@@ -2,13 +2,14 @@ package springmongoapp.bookStore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/index")
-    public String index()
+    public ModelAndView index()
     {
-        return "index";
+        return new ModelAndView("index");
     }
 }
